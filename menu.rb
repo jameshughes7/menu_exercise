ab13fc8499a8edaa9dc80a2daab4261de25d542c
James Hughes <hugjimbo77@gmail.com>
2017-12-21 12:27:28 +0000

    added display_meal_price method

--- menu.rb
+++ menu.rb
@@ -1,6 +1,11 @@
+#Create a method to display the meals on the menu
 def display_menu(menu_hash)
   puts "Here is a selection of our meals to choose from: "
   menu_hash.each { |meal, price| puts meal }
+end
+#Create a method to select the price of a select meal
+def display_meal_price(menu_hash, meal)
+  menu_hash[meal]
 end
 
 #Create a menu hash
@@ -10,6 +15,7 @@ menu = { 'Lasagne' => 7,
           'Burrito' => 9
         }
 
+#Create user input to ask for the price of a meal on the menu
 loop do
   puts "Would you like to know the price of a meal?(y/n)"
   answer = gets.chomp
@@ -22,16 +28,8 @@ loop do
   answer = gets.chomp
 
   if menu.include?(answer)
-    puts "The price of #{answer} is price"
+    puts "The price of #{answer} is #{display_meal_price(menu, answer)}"
   else
     puts "Sorry that is not a dish we have on the menu."
   end
 end
-
-
-
-
-
-#Create user input to ask for the price of a meal on the menu
-#Create a method to display the meals on the menu
-#Create a method to select the price of a select meal
